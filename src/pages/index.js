3863a6c3bb8473078efeb2be3b4fc6d0b764cee3
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-04-29 19:33:41 -0400

    center sketch on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,21 @@
 import React from "react"
+import styled from "styled-components"
+import "../styles/main.css"
 
+import { Layout } from "../components/Layout/Layout"
 import Sketch from "../components/Sketch"
 
 export default () => (
-  <div>
-    <Sketch></Sketch>
-  </div>
+  <Layout>
+    <SketchContainer>
+      <Sketch></Sketch>
+    </SketchContainer>
+  </Layout>
 )
+
+const SketchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`
