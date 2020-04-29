82111eb3aec5439aa40d28afe0ad9443fd2b129f
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-04-29 17:19:58 -0400

    add whirlin perlin sketch

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -8,9 +8,50 @@ export default class Sketch extends Component {
   }
 
   Sketch = p => {
-    p.setup = () => {}
+    p5.disableFriendlyErrors = true
 
-    p.draw = () => {}
+    let scale = 15
+    let inc = 0.025
+    let zoff = 0
+    let cols, rows
+
+    p.setup = () => {
+      p.createCanvas(p.windowWidth / 2, p.windowHeight / 2)
+      cols = Math.floor(p.width / scale)
+      rows = Math.floor(p.height / scale)
+    }
+
+    p.draw = () => {
+      p.fill(255, 5)
+      p.noStroke()
+      p.rect(0, 0, p.width, p.height)
+
+      p.perlinGrid()
+    }
+
+    p.perlinGrid = () => {
+      p.stroke(0)
+      p.strokeWeight(0.1)
+      let yoff = 0
+
+      for (let y = 0; y < rows; y++) {
+        let xoff = 0
+        for (let x = 0; x < cols; x++) {
+          xoff += inc
+          let angle = p.noise(xoff, yoff, zoff) * p.TWO_PI
+          let v = p5.Vector.fromAngle(angle)
+
+          p.push()
+          p.translate(x * scale, y * scale)
+          p.rotate(v.heading())
+          p.line(0, 0, scale * p.map(angle, 2, 4, 0, 2), 0)
+          p.pop()
+        }
+        yoff += inc
+        zoff += 0.0001
+        inc = p.noise(yoff, zoff) * 0.1
+      }
+    }
   }
 
   componentDidMount() {
