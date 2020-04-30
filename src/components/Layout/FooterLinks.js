7152645dc43729a5bc44471e708eeab5dfa8277b
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-04-29 23:00:36 -0400

    add footer links component w/ icons

---
+++ src/components/Layout/FooterLinks.js
@@ -0,0 +1,58 @@
+import React from "react"
+import styled from "styled-components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faGithubSquare,
+  faLinkedin,
+  faTwitterSquare,
+  faInstagramSquare,
+} from "@fortawesome/free-brands-svg-icons"
+
+export const FooterLinks = () => {
+  return (
+    <Container>
+      <Container>
+        <IconLink href="#" target="_blank" rel="noopener noreferrer">
+          <Icon icon={faGithubSquare}></Icon>
+        </IconLink>
+
+        <IconLink href="#" target="_blank" rel="noopener noreferrer">
+          <Icon icon={faLinkedin}></Icon>
+        </IconLink>
+
+        <IconLink href="#" target="_blank" rel="noopener noreferrer">
+          <Icon icon={faTwitterSquare}></Icon>
+        </IconLink>
+
+        <IconLink href="#" target="_blank" rel="noopener noreferrer">
+          <Icon icon={faInstagramSquare}></Icon>
+        </IconLink>
+      </Container>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 2rem;
+  display: flex;
+`
+
+const Icon = styled(FontAwesomeIcon)`
+  font-size: 3.2rem;
+`
+
+const IconLink = styled.a`
+  color: #1c1c1c;
+  text-decoration: none;
+
+  :not(:last-child) {
+    margin-right: 2rem;
+  }
+
+  :hover {
+    color: #5c5c5c;
+  }
+`
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from "react"
 import styled from "styled-components"
 
 import { Header } from "./Header"
+import { FooterLinks } from "./FooterLinks"
 
 export const Layout = ({ children }) => {
   return (
     <LayoutWrapper>
       <Header></Header>
       {children}
+      <FooterLinks></FooterLinks>
     </LayoutWrapper>
   )
 }
